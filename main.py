831ae81fd588a8cb5d1fdac703673d2717e27b06
Miraculin <colton.cheung@hotmail.ca>
2018-08-23 01:05:11 -0700

    added dataset, updated requirements

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+
+censusFilePath = "../datasets/census2018.csv"
 sb.set(style = 'darkgrid')
+census = pd.read_csv(censusFilePath)
+print(census)
 plt.show()
